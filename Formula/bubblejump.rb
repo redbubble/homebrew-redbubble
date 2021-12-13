e9c581b9b0edc76956ff9a39c4416de60e701b7b
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-13 12:06:29 +1100

    Brew formula update for bubblejump version v0.6.1

--- Formula/bubblejump.rb
+++ Formula/bubblejump.rb
@@ -5,12 +5,12 @@
 class Bubblejump < Formula
   desc "Quickly get shell access to EC2 instances in our VPC"
   homepage "https://github.com/redbubble/bubblejump"
-  version "0.6.0"
+  version "0.6.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "http://apt.redbubble.com/bubblejump/v0.6.0/bubblejump_0.6.0_Darwin_arm64.tar.gz"
-      sha256 "27cc07fef04ae487b92a9df73a30113486447cec37edbdd5979f9223404e126c"
+      url "http://apt.redbubble.com/bubblejump/v0.6.1/bubblejump_0.6.1_Darwin_arm64.tar.gz"
+      sha256 "238f70bba6f65f99441760c6b4472810c462715aead56927cc1552be03808c4b"
 
       def install
         bin.install "bubblejump"
@@ -19,8 +19,8 @@ class Bubblejump < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "http://apt.redbubble.com/bubblejump/v0.6.0/bubblejump_0.6.0_Darwin_x86_64.tar.gz"
-      sha256 "8b4fedfded9105bb5c0ad22d23e42e6bcf8783334a5dca41f15cda7f47071f66"
+      url "http://apt.redbubble.com/bubblejump/v0.6.1/bubblejump_0.6.1_Darwin_x86_64.tar.gz"
+      sha256 "0845a936146cb76d56cdfd16cfdd0891a5490e6a25483db52f556e936ea88f1c"
 
       def install
         bin.install "bubblejump"
@@ -32,8 +32,8 @@ class Bubblejump < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://apt.redbubble.com/bubblejump/v0.6.0/bubblejump_0.6.0_Linux_arm64.tar.gz"
-      sha256 "e5426ac48a9c88bc8d360d79a565585e75f3fcec8df22363f1dee1f55e36d13c"
+      url "http://apt.redbubble.com/bubblejump/v0.6.1/bubblejump_0.6.1_Linux_arm64.tar.gz"
+      sha256 "06ff7fed535350bae1b8c1d1a75364ad906fbb00ab8b705b124655ccbbcbd749"
 
       def install
         bin.install "bubblejump"
@@ -42,8 +42,8 @@ class Bubblejump < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "http://apt.redbubble.com/bubblejump/v0.6.0/bubblejump_0.6.0_Linux_x86_64.tar.gz"
-      sha256 "d737fb944715eaff45f4c335db0d9de95a53c95308200bfc8bfd0d27811d5993"
+      url "http://apt.redbubble.com/bubblejump/v0.6.1/bubblejump_0.6.1_Linux_x86_64.tar.gz"
+      sha256 "e2c1686ad2515ad336ac366613573ca78d958dbb6f084ddb7206c2aac9f9fd20"
 
       def install
         bin.install "bubblejump"
